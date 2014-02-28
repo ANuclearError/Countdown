283fb8f09b261c5ec7bb83a9a831456cc1857f05
Aidan O'Grady <theaidanogrady@gmail.com>
2014-02-28 21:20:04 +0000

    Added the driver file. Currently prints placeholder.

---
+++ src/Driver.java
@@ -0,0 +1,16 @@
+/**	
+ * @author	Aidan O'Grady
+ * @version	0.0
+ * @since	28/02/2014
+ */
+public class Driver{
+	/** 
+	 * The main method to control the project.
+	 * 
+	 * While currently holding a placeholder print out, this method will be used
+	 * for the initial execution of the game.	
+	 */
+	public static void main(String[] args){
+		System.out.println("Test print please ignore");
+	}
+}
