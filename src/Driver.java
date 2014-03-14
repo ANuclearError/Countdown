ad04bf91ea360668959e42db29816c8239ebf1fb
Aidan O'Grady <theaidanogrady@gmail.com>
2014-03-14 11:48:55 +0000

    Added basic menu

--- src/Driver.java
+++ src/Driver.java
@@ -11,6 +11,6 @@ public class Driver{
 	 * for the initial execution of the game.	
 	 */
 	public static void main(String[] args){
-		System.out.println("Test print please ignore");
+		Countdown game = new Countdown();
 	}
 }
